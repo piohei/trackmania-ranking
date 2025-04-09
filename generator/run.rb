0169e0353ad92f174afa4f114561b2183efe957f
Piotr Heilman <piotr.heilman@gmail.com>
2025-04-09 16:19:43 +0200

    Improved styling. Updated index.

--- generator/run.rb
+++ generator/run.rb
@@ -59,7 +59,7 @@ user_id_to_name.keys.each { |user_id| get_data(res, user_id) }
 
 
 html_part = """
-<table>
+<table class=\"table table-striped table-hover\">
 <thead>
 <th>Map name</th>
 <th>Author</th>
@@ -87,11 +87,14 @@ res.keys.sort.each { |map_id|
 html_part += "</tbody>\n</table>\n"
 
 puts """
-<!DOCTYPE html>
-<html>
+<!doctype html>
+<html lang=\"en\">
   <head>
     <meta charset=\"utf-8\">
+    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">
     <title>Ranking</title>
+    <link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css\" rel=\"stylesheet\" integrity=\"sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7\" crossorigin=\"anonymous\">
+    <script src=\"https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js\" integrity=\"sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq\" crossorigin=\"anonymous\"></script>
   </head>
   <body>
     <h1>Ranking</h1>
